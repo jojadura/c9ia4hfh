20aef3eeeec2e2ac2abad75286a36de4ef45f02d
john j <as@gmail.com>
2015-11-24 17:51:37 -0500

    Deploy

--- db/migrate/20150510175923_create_rooms.rb
+++ db/migrate/20150510175923_create_rooms.rb
@@ -8,5 +8,6 @@ class CreateRooms < ActiveRecord::Migration
 
       t.timestamps null: false
     end
+    add_column :rooms, :price_per_night, :float
   end
 end
--- db/migrate/201511241727_add_column_price_to_rooms.rb
+++
@@ -1,5 +0,0 @@
-class AddColumnPriceToRooms < ActiveRecord::Migration
-  def change
-    add_column :rooms, :price_per_night, :float
-  end
-end
