fc98977cf1d1a0db38c76166aeb1855833acee31
john j <as@gmail.com>
2015-11-24 16:55:25 -0500

    Implementar Validaciones

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,4 +13,13 @@
 #
 
 class Room < ActiveRecord::Base
+
+	validates :title, presence: true
+	validates :description, presence: true
+	validates :beds, presence: true, numericality: true, :numericality => {:only_integer => true}
+	validates :guests, presence: true, :numericality => {:only_integer => true}
+	validates :image_url, presence: true
+
+	validates :description, length: { in: 1..400 }, allow_nil: true
+	
 end
